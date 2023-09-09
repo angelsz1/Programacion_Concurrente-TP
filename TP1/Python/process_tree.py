93ff2cf8a895c21b6d20391787c36c46b45bf09b
facuegon481 <63320772+facuegon481@users.noreply.github.com>
2023-09-09 19:50:56 -0300

    showProcess method

--- TP1/Python/process_tree.py
+++ TP1/Python/process_tree.py
@@ -1,31 +1,34 @@
 import os
 import time
 
+def showProcess(process_name):
+    print(f"Proceso {process_name} - PID: {os.getpid()} - PID padre: {os.getppid()}")
+    
 pid_A = os.getpid()
 print(f"\nProceso A - PID: {pid_A}")
 
 pid_B = os.fork()
 if pid_B == 0:
     #Proceso B
-    print(f"Proceso B - PID: {os.getpid()} - PID padre: {os.getppid()}")
+    showProcess("B")
     pid_D = os.fork()
     if(pid_D == 0):
         #Proceso D
-        print(f"Proceso D - PID: {os.getpid()} - PID padre: {os.getppid()}")
+        showProcess("D")
     else:
         #Proceo B (padre)
         pid_E = os.fork()
         if(pid_E == 0):
             #Proceso E
-            print(f"Proceso E - PID: {os.getpid()} - PID padre: {os.getppid()}")
+            showProcess("E")
             pid_G = os.fork()
             if(pid_G == 0):
-                print(f"Proceso G - PID: {os.getpid()} - PID padre: {os.getppid()}")
+                showProcess("G")
             else:
                 #Proceso E (padre)
                 pid_H = os.fork()
                 if(pid_H == 0):
-                    print(f"Proceso H - PID: {os.getpid()} - PID padre: {os.getppid()}")
+                    showProcess("H")
                 else:
                     os.wait()
         else:
@@ -35,10 +38,10 @@ else:
     #Proceso A
     pid_C = os.fork()
     if(pid_C == 0):
-        print(f"Proceso C - PID: {os.getpid()} - PID padre: {os.getppid()}")
+        showProcess("C")
         pid_F = os.fork()
         if(pid_F == 0):
-            print(f"Proceso F - PID: {os.getpid()} - PID padre: {os.getppid()}")
+            showProcess("F")
         else:
             os.wait()
     else:
